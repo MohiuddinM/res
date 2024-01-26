e35ac35a8571e212e37762addaee231ef0104218
Muhammad Mohiuddin <muhammad.mohiuddin@live.com>
2024-01-27 00:27:56 +0100

    fromFuture can take functions too
    - add comments

--- lib/src/result.dart
+++ lib/src/result.dart
@@ -2,23 +2,39 @@ import 'dart:async';
 
 import 'package:meta/meta.dart';
 
+/// Result class that can either contain a value on success,
+/// or an error on failure
 @immutable
 class Result<R, S> {
+  /// Create a result with a [value]
   const Result.ok([this._value])
       : assert(_value != null || Future<R> == Future<void>),
         _error = null;
 
+  /// Create a Result with an [error]
   const Result.err(S error)
       : assert(Future<S> != Future<void>),
         _value = null,
         _error = error;
 
+  /// Create a [Result] from a [Future].
+  ///
+  /// If [future] completes successfully then [Result] will contain its value,
+  /// and if the [future] fails then its error will be contained in [Result]
   static Future<Result<R, S>> fromFuture<R, S>(
-    Future<R> future, {
-    required S Function(dynamic, StackTrace) error,
+    dynamic future, {
+    required S Function(dynamic e, StackTrace s) error,
   }) async {
+    assert(
+      future is Future<R> || future is Future<R> Function(),
+      'future can only be a "Future<$R>" or a "Future<$R> Function()"',
+    );
     try {
-      return Result.ok(await future);
+      return Result.ok(
+        future is Future<R>
+            ? await future
+            : await (future as Future<R> Function()).call(),
+      );
     } catch (e, s) {
       return Result.err(error(e, s));
     }
@@ -27,10 +43,15 @@ class Result<R, S> {
   final R? _value;
   final S? _error;
 
+  /// Check if this [Result] contains an error
   bool get isError => _error != null;
 
+  /// Get the value from this [Result]. This will throw error if this [Result]
+  /// is an error. Check [isError] before accessing this.
   R get value => _value!;
 
+  /// Get the error from this [Result]. This will throw error if this [Result]
+  /// has a value instead. Check [isError] before accessing this.
   S get error => _error!;
 
   @override
